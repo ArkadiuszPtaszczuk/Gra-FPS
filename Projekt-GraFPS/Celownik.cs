b78f2972735f3bd6d191327cf4ac72abc53a1625
Arkadiusz Ptaszczuk <arek230493@gmail.com>
2017-04-25 20:53:25 +0200

    .

---
+++ Projekt-GraFPS/Celownik.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Celownik : MonoBehaviour
+{
+
+
+    public bool rysujCelownik = true;
+    public Color crosshairColor = Color.white;
+    public float width = 2;
+    public float rozmiarCel = 2;
+    private Texture2D tex;
+    private float newHeight;
+    private GUIStyle lineStyle;
+
+    void Awake()
+    {
+        tex = new Texture2D(1, 1);
+        lineStyle = new GUIStyle();
+        lineStyle.normal.background = tex;
+    }
+
+
+    void OnGUI()
+    {
+        Vector2 punktCentralny = new Vector2(Screen.width / 2, Screen.height / 2);
+        float screenRatio = Screen.height / 150;
+        newHeight = rozmiarCel * screenRatio;
+
+        if (rysujCelownik)
+        {
+            GUI.Box(new Rect(punktCentralny.x - (width / 2), punktCentralny.y - (newHeight), width, newHeight), GUIContent.none, lineStyle);
+            GUI.Box(new Rect(punktCentralny.x - (width / 2), (punktCentralny.y), width, newHeight), GUIContent.none, lineStyle);
+            GUI.Box(new Rect((punktCentralny.x), (punktCentralny.y - (width / 2)), newHeight, width), GUIContent.none, lineStyle);
+            GUI.Box(new Rect(punktCentralny.x - (newHeight), (punktCentralny.y - (width / 2)), newHeight, width), GUIContent.none, lineStyle);
+        }
+
+        kolorCelownika(tex, crosshairColor);
+    }
+
+    void kolorCelownika(Texture2D myTexture, Color myColor)
+    {
+        for (int y = 0; y < myTexture.height; y++)
+        {
+            for (int x = 0; x < myTexture.width; x++)
+            {
+                myTexture.SetPixel(x, y, myColor);
+            }
+            myTexture.Apply();
+        }
+    }
+
+}
---
+++ Projekt-GraFPS/DrzwiNew.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DrzwiNew : MonoBehaviour
+{
+
+    private Animator animator;
+    bool doorIsOpen = false;
+    public Text otworz;
+    public Canvas komunikaty;
+    // Use this for initialization
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        doorIsOpen = false;
+        obsluzKomunikat(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player Enter");
+        if (other.tag == "Player")
+        {
+            obsluzKomunikat(true);
+            if (doorIsOpen == false)
+            {
+
+                ustawKomunikat(false);
+            }
+            else
+            {
+
+                ustawKomunikat(true);
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Player Exit");
+        if (other.tag == "Player")
+        {
+            obsluzKomunikat(false);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Player Stay");
+        animator = gameObject.GetComponent<Animator>();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E");
+
+            if (doorIsOpen == false)
+            {
+                animator.SetTrigger("Otworz");
+                doorIsOpen = true;
+            }
+            else
+            {
+                animator.SetTrigger("Zamknij");
+                doorIsOpen = false;
+            }
+
+        }
+
+    }
+    private void obsluzKomunikat(bool val)
+    {
+        komunikaty.enabled = val;
+
+    }
+
+    private void ustawKomunikat(bool val)
+    {
+        if (otworz != null)
+        {
+            if (doorIsOpen == false)
+            {
+                otworz.text = "E aby otworzyć";
+            }
+            else
+            {
+                otworz.text = "E aby zamknąć";
+            }
+        }
+    }
+
+
+
+
+}
